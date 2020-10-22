2995d181be3d0076fd4d8116fd2cf715ca801e37
Mladen <mladengeorgiev18@abv.bg>
2020-10-22 17:14:12 +0300

    deleting some files

--- capitals.py
+++
@@ -1,6 +0,0 @@
-countries = input().split(", ")
-capitals = input().split(", ")
-
-tuples = list(zip(countries,capitals))
-for duos in tuples:
-    print(f"{duos[0]} -> {duos[1]}")
--- even_len.py
+++
@@ -1,4 +0,0 @@
-
-even_len = [word for word in input().split() if len(word) % 2 == 0]
-for word in even_len:
-    print(word)
