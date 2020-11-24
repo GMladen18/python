1b2aef0d85c025adcfcb96ce217cccaae417b9c9
Mladen Georgiev <70316906+GMladen18@users.noreply.github.com>
2020-11-24 15:22:20 +0200

    Update main.py

--- email_sender/CPU_reporter/main.py
+++ email_sender/CPU_reporter/main.py
@@ -1,22 +1,22 @@
 from datetime import timedelta
 from email_sender import send_email
 from report_formatter import format_diagram_report
-from report_generator import generate_report
+from report_creator import create_report
 
 
 class Reporter:
-    def __init__(self, generator, formatter, sender):
-        self._generator = generator
+    def __init__(self, creator, formatter, sender):
+        self._creator = creator
         self._formatter = formatter
         self._sender = sender
         self.duration = timedelta(seconds=2)
 
     def send_mail(self):
-        report = self._generator(self.duration)
+        report = self._creator(self.duration)
         formatted_report = self._formatter(report)
         self._sender(formatted_report)
         print("Email sent!")
 
 
-cpu_report = Reporter(generate_report, format_diagram_report, send_email)
+cpu_report = Reporter(create_report, format_diagram_report, send_email)
 cpu_report.send.mail()
