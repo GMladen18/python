716226daabd49c75eda033d03a31d157a823d1d0
Mladen <mladengeorgiev18@abv.bg>
2021-05-13 17:18:21 +0300

    add wether_app

---
+++ wether_app/weather/views.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.conf.urls import url
+from django.shortcuts import render
+import requests
+
+from weather.models import City
+from weather.forms import CityForm
+
+
+# Create your views here.
+
+def index_view(request, *args, **kwargs):
+    cities = City.objects.all()
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=6bd02d1ff7c484bb30c023666e9004e4'
+
+    if request.method == 'POST':
+        print(request)
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    form = CityForm()
+
+    weather_data = []
+
+    for city in cities:
+        response = requests.get(url.format(city.name))
+        if response.ok:
+            city_weather = response.json()
+        else:
+            break
+           
+        temp = city_weather['main']['temp']
+
+        
+        weather = {
+            'city': city,
+            'temperature': temp,
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather)
+    context = {'weather_data': weather_data, 'form': form }
+    return render(request, 'index.html', context)
+
+
+
